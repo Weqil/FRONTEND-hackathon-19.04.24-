f33af7c829d32e855e46084b9332587740a92c87
Weqil <ar5ipooshka@yandex.ru>
2024-04-20 21:14:04 +0500

    statistic company

--- src/app/routing/private-routes.ts
+++ src/app/routing/private-routes.ts
@@ -4,9 +4,9 @@ import { CabinetComponent } from '../views/cabinet/cabinet.component';
 
 
 export const privateRoutes: Routes = [
-  {
-    path: 'cabinet',
-    component: CabinetComponent,
-    canActivate: [AuthGuard],
-  }
+  // {
+  //   path: 'cabinet',
+  //   component: CabinetComponent,
+  //   canActivate: [AuthGuard],
+  // }
 ];
