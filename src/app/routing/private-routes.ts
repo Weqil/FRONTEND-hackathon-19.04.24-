6e2acdb5d770181100bbe9a86271a04452dbe248
MoskowPsix <bolshe.kivi@gmail.com>
2024-04-21 15:21:36 +0500

    add functional

--- src/app/routing/private-routes.ts
+++ src/app/routing/private-routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AuthGuard } from '../guards/auth.guard';
 import { CabinetComponent } from '../views/cabinet/cabinet.component';
+import { WorkerProfileComponent } from '../views/worker-profile/worker-profile.component';
+import { CheckAuthCanActiveGuard } from '../guards/check-auth.canactive.guard';
 
 
 export const privateRoutes: Routes = [
@@ -8,5 +10,15 @@ export const privateRoutes: Routes = [
     path: 'cabinet',
     component: CabinetComponent,
     canActivate: [AuthGuard],
-  }
+  },
+  {
+    path:'worker-profile',
+    component:WorkerProfileComponent,
+    canActivate: [CheckAuthCanActiveGuard]
+  },
+  {
+    path:'worker-profile/:id',
+    component:WorkerProfileComponent,
+    canActivate: [CheckAuthCanActiveGuard]
+  },
 ];
