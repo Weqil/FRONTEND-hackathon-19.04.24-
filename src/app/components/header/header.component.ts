374dae77d61ce5ceb3fe1b5b7f7493f15cffc6cc
Weqil <ar5ipooshka@yandex.ru>
2024-04-21 09:40:40 +0500

    change header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent  implements OnInit {
 
   constructor() { }
-  auth:boolean = false
+  auth:boolean = true
   ngOnInit() {}
 
 }
